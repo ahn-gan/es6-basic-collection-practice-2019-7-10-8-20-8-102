97b64c4070aa4604a7049f33ac1a398a51b876d6
Akin <akin.gan@oocl.com>
2019-07-11 13:07:26 +0800

    feat: section-2 practice-2 finish.

--- main/section-2/practice-2.js
+++ main/section-2/practice-2.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+    // 去重
+    let uniqueCollection = collection.filter((element, index, self) => self.indexOf(element) === index); 
+    let result = [];
+    uniqueCollection.map(val => {
+      let sameElements = collection.filter(item => item === val);
+      let valObj = {
+        key: val.indexOf('-') > -1 ? val.split('-')[0] : val,
+        count: sameElements[0].indexOf('-') > -1 ? parseInt(sameElements[0].split('-')[1]) : sameElements.length
+      };
+      result.push(valObj);
+    });
+    return result;
 }
