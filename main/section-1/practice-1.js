b2b55a979d7ba622de65fbdc50987b19a8ba9ee7
Akin <akin.gan@oocl.com>
2019-07-11 12:46:56 +0800

    feat: section-2 practice-1 finish.

--- main/section-1/practice-1.js
+++ main/section-1/practice-1.js
@@ -3,6 +3,6 @@
 module.exports = function collectSameElements(collectionA, collectionB) {
 
   return collectionA.filter( itemA => {
-    return itemA == collectionB.filter(itemB => itemB == itemA)[0];
-  })
+    return itemA === collectionB.filter(itemB => itemB === itemA)[0];
+  });
 }
