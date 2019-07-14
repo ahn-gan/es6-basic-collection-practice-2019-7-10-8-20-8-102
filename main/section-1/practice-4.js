5f6a8449a3f8f8a244f56d3bfffe6739913ff2e3
Akin <akin.gan@oocl.com>
2019-07-14 16:12:29 +0800

    [refactor][refactor the code of section-1 practice-4 and remove some unused annotation]

--- main/section-1/practice-4.js
+++ main/section-1/practice-4.js
@@ -1,10 +1,9 @@
 'use strict';
 
 module.exports = function collectSameElements(collectionA, objectB) {
-  let result = [];
   let filterArray = collectionA.filter( itemA => {
     return itemA.key == objectB.value.filter(itemB => itemB == itemA.key)[0];
   });
-  filterArray.map(element => result.push(element.key));
+  let result = filterArray.map(element => element.key);
   return result;
 }
--- main/section-2/practice-2.js
+++ main/section-2/practice-2.js
@@ -3,15 +3,8 @@
 module.exports = function countSameElements(collection) {
     // 去重
     let uniqueCollection = collection.filter((element, index, self) => self.indexOf(element) === index); 
-    // let result = [];
     let result = uniqueCollection.map(val => {
       let sameElements = collection.filter(item => item === val);
-      
-      // let valObj = {
-      //   key: val.indexOf('-') > -1 ? val.split('-')[0] : val,
-      //   count: sameElements[0].indexOf('-') > -1 ? parseInt(sameElements[0].split('-')[1]) : sameElements.length
-      // };
-      // result.push(valObj);
       return {
         key: val.indexOf('-') > -1 ? val.split('-')[0] : val,
         count: sameElements[0].indexOf('-') > -1 ? parseInt(sameElements[0].split('-')[1]) : sameElements.length
