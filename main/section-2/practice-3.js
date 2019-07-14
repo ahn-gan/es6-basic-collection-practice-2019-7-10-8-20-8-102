9db82d0264b8dfcc88405bc5189483d671b46d73
Akin <akin.gan@oocl.com>
2019-07-14 18:14:32 +0800

    [refactor][code refactor for section-2 practice-3]

--- main/section-2/practice-3.js
+++ main/section-2/practice-3.js
@@ -1,30 +1,21 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  let elementMap = new Map();
-  collection.forEach(item => {
-    let count = 0;
-    // 使用正则表达式判断字符串是否包含数字
-    let regExp = /[0-9]/;
-    if (regExp.test(item)) {
-      // 包含数字则取出数字
-      let keyCount = parseInt(item.replace(/[^0-9]/ig,''));
-      // 取出key
-      let key = item.substr(0, 1);
-      count = (elementMap.get(key) === undefined) ? keyCount : elementMap.get(key) + keyCount;
-      elementMap.set(key, count);
-    } else {
-      count = (elementMap.get(item) === undefined) ? 1 : elementMap.get(item) + 1;
-      elementMap.set(item, count);
-    }
-  });
-  let result = [];
-  elementMap.forEach((value, key) => {
-    let item = {
-      name: key,
-      summary: value
-    };
-    result.push(item);
-  });
-  return result;
+      let regExp = /[0-9]/;
+      let normalCollection = collection.map(element => regExp.test(element) ? { name: element.substr(0, 1), summary: parseInt(element.replace(/[^0-9]/ig,'')) } : {name: element, summary: 1});
+      let elementMap = new Map();
+
+      normalCollection.map(element => {
+        let count = (elementMap.get(element.name) === undefined) ? element.summary : elementMap.get(element.name) + element.summary;
+        elementMap.set(element.name, count);
+      });
+      let result = [];
+      elementMap.forEach((value, key) => {
+        let item = {
+          name: key,
+          summary: value
+        };
+        result.push(item);
+      });
+      return result;
 }
