d1334c084a1f2c39047d868730e0eed3af490251
Akin <akin.gan@oocl.com>
2019-07-14 16:08:18 +0800

    [refactor][refactor the code of section-2 practice-1]

--- main/section-2/practice-1.js
+++ main/section-2/practice-1.js
@@ -3,14 +3,12 @@
 module.exports = function countSameElements(collection) {
   // 去重
   let uniqueCollection = collection.filter((element, index, self) => self.indexOf(element) === index); 
-  let result = [];
-  uniqueCollection.map(val => {
+  let result = uniqueCollection.map(val => {
     let sameElements = collection.filter(item => item === val);
-    let valObj = {
+    return {
       key: val,
       count: sameElements.length
     };
-    result.push(valObj);
   });
   return result;
 }
